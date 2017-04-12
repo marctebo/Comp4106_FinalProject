357b0c8f519294661c49cdcec1eb21b1a2a64766
marctebo <marctebo@gmail.com>
2017-04-12 12:35:16 -0400

    Generated all moves for state

--- COMP4106_FinalProject/src/Boat.java
+++ COMP4106_FinalProject/src/Boat.java
@@ -3,16 +3,22 @@ import java.util.ArrayList;
 
 
 public class Boat {
-	public final int HORIZ = 1;
-	public final int VERT = 2;
+	public static final int HORIZ = 1;
+	public static final int VERT = 2;
 	private ArrayList<Dimension> area;
-	private int id;
-	private int orient;
-	
+	private int id, orient, forward, backward;
+	private Dimension front,back;
+	private boolean goalBoat = false;
+
 	public Boat(int id){
 		this.id = id;
 		area = new ArrayList<Dimension>();
+		if(id==1){
+			goalBoat = true;
+		}
 	}
+	
+
 	public ArrayList<Dimension> getArea() {
 		return area;
 	}
@@ -25,6 +31,44 @@ public class Boat {
 	public void setId(int id) {
 		this.id = id;
 	}
+	public int getOrient() {
+		return orient;
+	}
+	public void setOrient(int orient) {
+		this.orient = orient;
+	}	
+	public int getForward() {
+		return forward;
+	}
+	public void setForward(int forward) {
+		this.forward = forward;
+	}
+	public int getBackward() {
+		return backward;
+	}
+	public void setBackward(int backward) {
+		this.backward = backward;
+	}
+	public Dimension getFront() {
+		return front;
+	}
+	public void setFront(Dimension front) {
+		this.front = front;
+	}
+	public Dimension getBack() {
+		return back;
+	}
+	public void setBack(Dimension back) {
+		this.back = back;
+	}
+	public boolean isGoalBoat() {
+		return goalBoat;
+	}
+
+	public void setGoalBoat(boolean goalBoat) {
+		this.goalBoat = goalBoat;
+	}
+
 	public void findOrient(){
 		if(area.get(0).getWidth()==area.get(1).getWidth()){
 			orient = HORIZ;
@@ -33,17 +77,67 @@ public class Boat {
 			orient = VERT;
 		}
 	}
-	public int getOrient() {
-		return orient;
+	public void setFrontBack(){
+		if(orient == HORIZ){
+			back = area.get(0);
+			front = area.get(area.size()-1);
+		}
+		else{
+			front = area.get(0);
+			back = area.get(area.size()-1);
+		}
 	}
-	public void setOrient(int orient) {
-		this.orient = orient;
+	public void generateFreeSpace(int[][] state,int size){
+		forward = 0;
+		backward = 0;
+		if(orient == HORIZ){
+			for(int j=(int)front.getHeight()+1;j<size;j++){
+				int i = (int)front.getWidth();
+				if(state[i][j]==0 || state[i][j]==-1){
+					forward++;
+				}
+				else{
+					break;
+				}
+			}
+			for(int j=(int)back.getHeight()-1;j>=0;j--){
+				int i = (int)back.getWidth();
+				if(state[i][j]==0 || state[i][j]==-1){
+					backward++;
+				}
+				else{
+					break;
+				}
+			}
+		}
+		
+		else{
+			for(int i=(int)front.getWidth()-1;i>=0;i--){
+				int j = (int)front.getHeight();
+				if(state[i][j]==0 || state[i][j]==-1){
+					forward++;
+				}
+				else{
+					break;
+				}
+			}
+			for(int i=(int)back.getWidth()+1;i<size;i++){
+				int j = (int)back.getHeight();
+				if(state[i][j]==0 || state[i][j]==-1){
+					backward++;
+				}
+				else{
+					break;
+				}
+			}
+		}
 	}
 	public static void main(String args[]){
 		Boat b = new Boat(1);
 		b.getArea().add(new Dimension(1,1));
 		b.getArea().add(new Dimension(2,1));
 		b.findOrient();
-		System.out.println(b.getOrient());
+		b.setFrontBack();
+		System.out.println(b.getFront() + "    " + b.getBack());
 	}
 }
--- COMP4106_FinalProject/src/ShipNode.java
+++ COMP4106_FinalProject/src/ShipNode.java
@@ -4,16 +4,18 @@ import java.util.ArrayList;
 
 public class ShipNode {
 
-	public final int size = 6;
+	public static final int size = 6;
 	private int[][] state;
 	private ShipNode parent;
 	private ArrayList<Boat> boats;
 	private ArrayList<ShipNode> moves;
+	private Dimension goal;
 	
 	public ShipNode(int[][] state,ShipNode parent){
 		this.state = state;
 		this.parent = parent;
 		getBoats();
+		getGoal();
 	}
 	
 	
@@ -21,7 +23,7 @@ public class ShipNode {
 		boats = new ArrayList<Boat>();
 		for(int i=0;i<size;i++){
 			for(int j=0;j<size;j++){
-				if(state[i][j]!=0){
+				if(state[i][j]>0){
 					if(!containsId(state[i][j])){
 						Boat b = new Boat(state[i][j]);
 						b.getArea().add(new Dimension(i,j));
@@ -32,6 +34,11 @@ public class ShipNode {
 					}
 				}
 			}
+		}
+		for(Boat b: boats){
+			b.findOrient();
+			b.setFrontBack();
+			b.generateFreeSpace(state, size);
 		}
 		return boats;
 	}
@@ -54,22 +61,98 @@ public class ShipNode {
 		return null;
 	}
 	
+	public Dimension getGoal(){
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				if(state[i][j] == -1){
+					goal = new Dimension(i,j);
+				}
+			}
+		}
+		return goal;
+	}
 	public ArrayList<ShipNode> getMoves(){
 		moves = new ArrayList<ShipNode>();
 		
 		for(Boat b: boats){
-			
+			if(b.getOrient()== Boat.HORIZ){
+				for(int j=0;j<b.getForward();j++){	//getting forward horizontal moves
+					int[][] temp = copyArray(state);
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()][(int)d.getHeight()] = 0;
+					}
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()][(int)d.getHeight()+j+1] = b.getId();
+					}
+					moves.add(new ShipNode(temp,this));
+				}
+				for(int j=0;j<b.getBackward();j++){	//getting backwards horizontal moves
+					int[][] temp = copyArray(state);
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()][(int)d.getHeight()] = 0;
+					}
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()][(int)d.getHeight()-j-1] = b.getId();
+					}
+					moves.add(new ShipNode(temp,this));
+				}
+			}
+			else{
+				for(int i=0;i<b.getForward();i++){	//getting forward vertical moves
+					int[][] temp = copyArray(state);
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()][(int)d.getHeight()] = 0;
+					}
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()-i-1][(int)d.getHeight()] = b.getId();
+					}
+					moves.add(new ShipNode(temp,this));
+				}
+				for(int i=0;i<b.getBackward();i++){	//getting backwards vertical moves
+					int[][] temp = copyArray(state);
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()][(int)d.getHeight()] = 0;
+					}
+					for(Dimension d: b.getArea()){
+						temp[(int)d.getWidth()+i+1][(int)d.getHeight()] = b.getId();
+					}
+					moves.add(new ShipNode(temp,this));
+				}
+			}
 		}
 		
 		return moves;
 	}
+	public void printState(){
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				System.out.print(state[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static int[][] copyArray(int[][] arr){
+		int[][] temp = new int[size][size];
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				temp[i][j] = arr[i][j];
+			}
+		}
+		return temp;
+	}
 	public static void main(String args[]){
 		int[][] temp = {{0,0,3,3,0,0},
 						{0,0,2,0,0,0},
-						{1,1,2,0,0,0},
-						{0,4,4,4,4,0},
+						{1,1,2,0,0,-1},
+						{0,0,0,4,4,0},
 						{0,0,0,0,0,0},
 						{0,0,5,5,0,0}};
 		ShipNode s = new ShipNode(temp,null);
+		Boat b = s.getBoats().get(3);
+		System.out.println(b.getId());
+		b.generateFreeSpace(temp, 6);
+		System.out.println(b.getForward() + "    " + b.getBackward());
+		
 	}
 }
